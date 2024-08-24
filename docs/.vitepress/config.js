157d9ba9685165d3bc9ba171cd4c2a211085cfe1
WHO53 <notwho53@gmail.com>
2024-08-25 02:23:54 +0530

    auto sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { createWriteStream } from "node:fs";
 import { resolve } from "node:path";
 import { SitemapStream } from "sitemap";
+import { generateSidebar } from "vitepress-sidebar";
 
 const links = [];
 
@@ -26,87 +27,7 @@ export default {
             {text: "Website", link: "https://droidian.org1"},
             {text: "GitHub", link: "https://github.com/droidian/droidian"}
         ],
-        sidebar: [
-            {
-                text: "Getting Started",
-                items: [
-                    {text: "Porting Guide", link: "/porting-guide"},
-                    {text: "Server-Side Integration", link: "/get-started/backend-integration"},
-                    {text: "Client Hints", link: "/get-started/client-hints"},
-                    {text: "Google Analytics Import", link: "/get-started/ga-import"},
-                    {text: "Plausible Analytics Import", link: "/get-started/plausible-import"},
-                    {text: "Fathom Analytics Import", link: "/get-started/fathom-import"},
-                    {text: "Exporting Statistics", link: "/get-started/export"},
-                    {text: "Troubleshooting", link: "/get-started/troubleshooting"}
-                ]
-            },
-            {
-                text: "Integrations",
-                items: [
-                    {text: "Caddy", link: "/integrations/caddy"},
-                    {text: "Docusaurus", link: "/integrations/docusaurus"},
-                    {text: "Framer", link: "/integrations/framer"},
-                    {text: "Gatsby", link: "/integrations/gatsby"},
-                    {text: "Google Search Console", link: "/integrations/search-console"},
-                    {text: "Google Tag Manager", link: "/integrations/tag-manager"},
-                    {text: "Laravel", link: "/integrations/laravel"},
-                    {text: "Shopify", link: "/integrations/shopify"},
-                    {text: "Webflow", link: "/integrations/webflow"},
-                    {text: "WordPress", link: "/integrations/wordpress"}
-                ]
-            },
-            {
-                text: "Advanced",
-                items: [
-                    {text: "Referrer and UTM Parameters", link: "/advanced/referrer-utm"},
-                    {text: "Tracking Conversion Goals", link: "/advanced/conversion-goals"},
-                    {text: "Tracking Events", link: "/advanced/events"},
-                    {text: "Tracking Outbound Links", link: "/advanced/outbound-links"},
-                    {text: "Tracking File Downloads", link: "/advanced/file-downloads"},
-                    {text: "Tracking 404 Errors", link: "/advanced/not-found-tracking"},
-                    {text: "Segmentation and A/B Testing", link: "/advanced/segmentation-tags"},
-                    {text: "Multi-Step Funnels", link: "/advanced/funnels"},
-                    {text: "Comparing Statistics", link: "/advanced/comparison"},
-                    {text: "Additional Domains and Rollup Views", link: "/advanced/domains-rollup"},
-                    {text: "Webhooks", link: "/advanced/webhooks"},
-                    {text: "Email Reports", link: "/advanced/email-reports"},
-                    {text: "Traffic Filter", link: "/advanced/traffic-filter"},
-                    {text: "Traffic Spike Notifications", link: "/advanced/traffic-spike"},
-                    {text: "Traffic Alerts", link: "/advanced/traffic-alerts"},
-                    {text: "Extending Sessions", link: "/advanced/sessions"},
-                    {text: "Proxying the Scripts", link: "/advanced/proxy"},
-                    {text: "Proxying Through Cloudflare Workers", link: "/advanced/cf-workers"},
-                    {text: "Access Management", link: "/advanced/access"},
-                    {text: "Embedding the Dashboard", link: "/advanced/embedding"},
-                    {text: "Theming", link: "/advanced/theme"},
-                    {text: "Custom Domains", link: "/advanced/custom-domains"}
-                ]
-            },
-            {
-                text: "API and SDKs",
-                items: [
-                    {text: "API Guide", link: "/api-sdks/api-guide"},
-                    {text: "API", link: "/api-sdks/api"},
-                    {text: "SDKs", link: "/api-sdks/sdks"}
-                ]
-            },
-            {
-                items: [
-                    {text: "FAQ", link: "/faq"},
-                    {text: "Affiliate Program", link: "/affiliate"},
-                    {text: "Billing", link: "/billing"},
-                    {text: "Privacy", link: "/privacy"},
-                    {text: "Changelog", link: "/changelog"}
-                ]
-            },
-            {
-                items: [
-                    {text: "Terms and Conditions", link: "https://pirsch.io/terms"},
-                    {text: "Privacy Policy", link: "https://pirsch.io/privacy"},
-                    {text: "Legal Notice", link: "https://pirsch.io/legal"}
-                ]
-            }
-        ]
+        sidebar: generateSidebar({}),
     },
     transformHtml: (_, id, { pageData }) => {
         if (!/[\\/]404\.html$/.test(id)) {
